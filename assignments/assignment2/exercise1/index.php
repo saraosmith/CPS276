89a1856f7861a44c7aabd1fca0db46ceffe63b6a
ssmith@domain.com <86253201+saraosmith@users.noreply.github.com>
2022-10-03 20:02:22 -0400

    updates

--- assignments/assignment2/exercise1/index.php
+++ assignments/assignment2/exercise1/index.php
@@ -24,7 +24,7 @@ $ul .= "</ul>";
 <!DOCTYPE html>
 <html lang="en">
     <head>
-      <title>Table</title>
+      <title>Nested List</title>
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1">
     </head>
